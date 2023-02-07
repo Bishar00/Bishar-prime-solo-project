ae66d2bea14a3b4065ee436a30ea22072441a5e5
Bishar00 <bishar1yusuf@gmail.com>
2023-02-07 11:07:16 -0600

    add materail ui nav bar

--- src/components/Nav2/Nav2.jsx
+++ src/components/Nav2/Nav2.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import TrendingUpSharpIcon from '@mui/icons-material/TrendingUpSharp';
+import PagesSharpIcon from '@mui/icons-material/PagesSharp';
+import VolunteerActivismSharpIcon from '@mui/icons-material/VolunteerActivismSharp';
+import CurrencyExchangeSharpIcon from '@mui/icons-material/CurrencyExchangeSharp';
+ function Nav2() {
+  const [value, setValue] = React.useState(0);
 
-function Nav2(){
-// const user = useSelector((store) => store.user);
-
-return (
-<div>
-    <div>
-        Home
-    </div>
-    <div>
-        Trending
-    </div>
-    <div>
-        Post
-    </div>
-    <div>
-        Donations
-    </div>
-    <div>
-        Subcription
-    </div>
-    </div>
-)
-
-
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Innovation" icon={<EmojiObjectsIcon />} />
+        <BottomNavigationAction label="Trending" icon={<TrendingUpSharpIcon />} />
+        <BottomNavigationAction label="Post" icon={<PagesSharpIcon />} />
+        <BottomNavigationAction label="Donations" icon={<VolunteerActivismSharpIcon />} />
+        <BottomNavigationAction label="Subscriptions" icon={<CurrencyExchangeSharpIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
 }
 
 export default Nav2;
